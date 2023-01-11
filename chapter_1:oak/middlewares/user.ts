2bcd735d94fc7a178eb3ca7d445fbe10536a8836
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2023-01-11 15:36:39 +0000

    error handling

--- chapter_1:oak/middlewares/user.ts
+++ chapter_1:oak/middlewares/user.ts
@@ -1,13 +1,29 @@
 // deno-lint-ignore-file ban-types
-import { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { Context, isHttpError, Status } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { nanoid } from "https://deno.land/x/nanoid@v3.0.0/mod.ts"
 
 export default {
   userid: async (ctx: Context, next: Function) => {
-    // do whatever checks to determine the user ID
-    ctx.state.userId = nanoid();
-    await ctx.cookies.set("lastVisit", new Date().toISOString(), { signed: true });
-    await next();
-    delete ctx.state.userId; // cleanup
+
+    try {
+      // do whatever checks to determine the user ID
+      ctx.state.userId = nanoid();
+      await ctx.cookies.set("lastVisit", new Date().toISOString(), { signed: true, sameSite: "lax" });
+      await next();
+      delete ctx.state.userId; // cleanup
+    } catch (err) {
+      if (isHttpError(err)) {
+        switch (err.status) {
+          case Status.NotFound:
+            // handle NotFound
+            break;
+          default:
+          // handle other statuses
+        }
+      } else {
+        // rethrow if you can't handle the error
+        throw err;
+      }
+    }
   }
 }
